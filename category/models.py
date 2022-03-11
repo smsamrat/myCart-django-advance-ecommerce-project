8672cad4f8ef7617a759576b5054aa6c186184a4
you <97931972+smsamrat@users.noreply.github.com>
2022-03-11 18:09:19 +0600

    Making category model

---
+++ category/models.py
@@ -0,0 +1,19 @@
+from tabnanny import verbose
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, unique=True)
+    describtion = models.TextField(max_length=300, blank =True)
+    cate_image = models.ImageField(upload_to='photos/categories/')
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category_name
+
